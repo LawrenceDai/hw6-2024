087cab952b42374baa06485ac6c89a6aabef7f8c
LawrenceDai <lawrend@umich.edu>
2024-04-09 15:43:32 -0400

    js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,37 @@
-var video;
+var video = document.querySelector("#player1");
+var volume = document.querySelector("#volume")
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
 
+	video.autoplay = false;
+	video.loop = false;
+	video.load();
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
 
+	volume.innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function () {
+	video.playbackRate *= 0.9;
+	console.log(video.playbackRate);
+})
+
+document.querySelector("#faster").addEventListener("click", function () {
+	video.playbackRate = video.playbackRate * 10 / 9;
+	console.log(video.playbackRate);
+})
+
+document.querySelector("#mute").addEventListener("click", function () {
+	video.muted = !video.muted;
+	this.innerHTML = (video.muted ? "Unmute" : "Mute");
+})
